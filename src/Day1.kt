adbbb77048da985b2da48824756fe5d060e93639
raimuhello <Limesyy@outlook.com>
2025-12-02 11:11:07 +0200

    Day 1

---
+++ src/Day1.kt
@@ -0,0 +1,50 @@
+import java.io.File
+import kotlin.math.abs
+
+fun main() {
+
+    val lines: List<String> = File("src\\Day1_input.txt").readLines()
+    var currentDegrees = 50
+    var password = 0
+
+    for (line in lines)
+    {
+        currentDegrees = turnDial(currentDegrees, line)
+        if (currentDegrees == 0)
+        {
+            password++
+        }
+    }
+    println(password)
+}
+
+
+fun turnDial(degrees: Int, instruction: String): Int {
+    val direction = instruction.take(1)
+    var valueText = instruction.drop(1)
+
+    while (valueText.length > 2)
+    {
+        valueText = valueText.drop(1)
+    }
+
+    val value = valueText.toInt()
+
+    var newDegrees: Int = degrees
+
+    if (direction == "L")
+    {
+        newDegrees -= value
+        if (newDegrees < 0)
+        {
+            newDegrees = 100 - abs(newDegrees)
+        }
+    } else {
+        newDegrees += value
+        if (newDegrees >= 100)
+        {
+            newDegrees -= 100
+        }
+    }
+    return newDegrees
+}
